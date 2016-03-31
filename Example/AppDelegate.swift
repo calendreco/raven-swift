3b23685d974b0ef3e8c7439ea53174df974c2c34
joshfisher <josh@liveapp.com>
2016-03-31 15:01:10 -0400

    adds UrlRequestBuffer
    and example usage

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -12,14 +12,20 @@ import Raven
 class AppDelegate: UIResponder, UIApplicationDelegate {
                             
     var window: UIWindow?
-
+    
+    var ravenBuffer = UrlRequestBuffer()
+    var ravenBufferFlushTask: UIBackgroundTaskIdentifier?
+    
+    static let FlushRavenBufferTaskIdentifier = "co.calendre.flush-raven-buffer-task"
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
       
         RavenClient.clientWithDSN("https://663998f40e734ea59087883feda37647:306481b9f6bb4a6287b334178d9f8c71@app.getsentry.com/4394")
-        
+
         RavenClient.sharedClient?.setupExceptionHandler()
+        
+        RavenClient.sharedClient?.transportDelegate = ravenBuffer
 
         return true
     }
@@ -30,8 +36,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationDidEnterBackground(application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        flushRavenBuffer(withApplication: application)
     }
 
     func applicationWillEnterForeground(application: UIApplication) {
@@ -43,9 +48,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillTerminate(application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        flushRavenBuffer(withApplication: application)
     }
-
-
+    
+    func flushRavenBuffer(withApplication application: UIApplication) {
+        ravenBufferFlushTask = application.beginBackgroundTaskWithName(AppDelegate.FlushRavenBufferTaskIdentifier, expirationHandler: nil)
+        ravenBuffer.flushBuffer() {
+            precondition(self.ravenBufferFlushTask != nil)
+            application.endBackgroundTask(self.ravenBufferFlushTask!)
+        }
+    }
 }
-
